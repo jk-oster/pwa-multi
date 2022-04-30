d0d7c55f9ac557e6daf71454ded44230d3e99f32
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2022-04-30 03:11:10 +0200

    Build Pipeline Setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
                 dest: 'app/js/pwa.js',
             },
         },
-        uglify: {
+        terser: {
             dist: {
                 files: {
                     'app/js/pwa.min.js': ['app/js/pwa.js'],
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
             },
             js: {
                 files: 'src/js/*.js',
-                tasks: ['concat', 'uglify'],
+                tasks: ['concat', 'terser'],
                 options: {
                     livereload: true,
                 },
@@ -36,10 +36,9 @@ module.exports = function(grunt) {
             dist: {                            // Target
                 options: {                       // Target options
                     style: 'expanded',
-                    
                 },
                 files: {                         // Dictionary of files
-                    'app/css/styles.css': 'src/scss/main.scss',       // 'destination': 'source'
+                    'app/css/main.css': 'src/scss/main.scss',       // 'destination': 'source'
                 },
             },
         },
@@ -98,10 +97,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-terser');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-uncss');
     grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin', 'concat', 'uglify']);
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin', 'concat', 'terser']);
 };
