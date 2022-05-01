902da134b2c6961d0cdd12ca77dab576116daa2b
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2022-05-01 20:28:27 +0200

    Added reroute logic and better login handling

--- app/views/view.login.js
+++ app/views/view.login.js
@@ -12,18 +12,19 @@ export let view = new KWM_Route("/login", async function () {
 });
 
 view.rendering = async function () {
-    await kwm.render("login", kwm.conf.appContainer, {});
-
     // Check if user is logged in
-    if (localStorage.token) {
-        // Forward user
+    if (localStorage.getItem('token') && localStorage.token !== 'undefined' && localStorage.getItem('chat')) {
         kwm.router.changeView('/app');
     }
-    // Render Login form
     else {
+        // Render Template
+        await kwm.render("login", kwm.conf.appContainer, {});
+
+        // Cache DOM
         view.DOM = {
             btn_login: document.querySelector('#btn_login'),
             btn_logout: document.querySelector('#btn_logout'),
+            form_login: document.querySelector('#frm_login'),
             input_username: document.querySelector('#username'),
             input_password: document.querySelector('#password'),
             user_display_name: document.querySelector('#user_display_name'),
@@ -31,24 +32,13 @@ view.rendering = async function () {
             msg_error: document.querySelector('#msg_error'),
         };
 
-        view.DOM.btn_login.addEventListener('click', async function (e) {
-            e.preventDefault(); // Prevent "Submit Action"
-            try {
-                await login(view.DOM.input_username.value, view.DOM.input_password.value);
-                view.DOM.user_display_name.innerText = kwm.t('welcome') + localStorage.display_name;
-                setTimeout(() => {
-                    kwm.router.changeView('/match');
-                }, 2500);
-            } catch (err) {
-                console.error(err);
-                view.DOM.msg_error.innerText = kwm.t('loginError');
-            }
-        });
+        // Register Listeners
+        kwm.utils.setSingleEventListener(view.DOM.btn_login, 'click', loginClickHandler);
     }
 };
 
 
-async function login(username = '', password = '') {
+async function fetchToken(username = '', password = '') {
     const headers = {
         'Content-Type': 'application/json'
     }
@@ -57,45 +47,31 @@ async function login(username = '', password = '') {
         password: password,
     });
     const response = await kwm.utils.apiPOST('/jwt-auth/v1/token', headers, body);
-    localStorage.setItem('token', response.token);
-    localStorage.setItem('id', response.id);
-    localStorage.setItem('nicename', response.nicename);
-    localStorage.setItem('first_name', response.first_name);
-    localStorage.setItem('last_name', response.last_name);
-    localStorage.setItem('display_name', response.display_name);
-    localStorage.setItem('description', response.description);
-    localStorage.setItem('group', response.group);
-    console.log(response);
+    if ('token' in response) {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('id', response.id);
+        localStorage.setItem('nicename', response.nicename);
+        localStorage.setItem('first_name', response.first_name);
+        localStorage.setItem('last_name', response.last_name);
+        localStorage.setItem('display_name', response.display_name);
+        localStorage.setItem('description', response.description);
+        localStorage.setItem('group', response.group);
+        console.log(response);
+    } else throw Error(response.message);
+}
 
-    const group = await kwm.utils.apiGET('/wp/v2/group/' + localStorage.group);
-    localStorage.setItem('course', group.acf.course[0]);
-    localStorage.setItem('country', group.acf.country);
-    localStorage.setItem('admin', group.acf.admin[0]);
-    localStorage.setItem('school', group.acf.school);
-    console.log(group);
-
-    const chats = await kwm.utils.apiGET('/wp/v2/chat?users=' + localStorage.id);
-    console.log(chats);
-    let userChat = { Chat: 'Has no user chat'};
-    if(!kwm.utils.isEmpty(chats)){
-        for(const chat of chats){
-            console.log(chat.acf);
-            for(const user of chat.acf.users){
-                console.log(user);
-                if(user.ID === Number(localStorage.id)){
-                    userChat = chat;
-                }
-            }
-        }
-        if(!kwm.utils.isEmpty(userChat)){
-            localStorage.setItem('chat', userChat.id);
-            localStorage.setItem('chatstart', userChat.acf.stardate);
-            localStorage.setItem('questions', JSON.stringify(userChat.acf.questions));
-            localStorage.setItem('tasks', JSON.stringify(userChat.acf.tasks));
-            const partners = userChat.acf.users.filter(user => user.ID !== Number(localStorage.id));
-            localStorage.setItem('partners', JSON.stringify(partners));
-        }
-        console.log(userChat);
+async function loginClickHandler(e) {
+    e.preventDefault(); // Prevent "Submit Action"
+    try {
+        localStorage.clear();
+        await fetchToken(view.DOM.input_username.value, view.DOM.input_password.value);
+        view.DOM.user_display_name.innerText = kwm.t('welcome') + " " + localStorage.display_name;
+        view.DOM.form_login.classList.add('visually-hidden');
+        setTimeout(() => {
+            kwm.router.changeView('/match');
+        }, 2500);
+    } catch (err) {
+        kwm.utils.showErrorMessage(err.message, view.DOM.msg_error);
     }
 }
 
