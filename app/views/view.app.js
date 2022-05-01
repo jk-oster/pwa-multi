8c82fea12933eb8c659022740c550da934d58df1
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2022-05-01 05:17:06 +0200

    Added clickhandler for Tasks and Questions

--- app/views/view.app.js
+++ app/views/view.app.js
@@ -12,5 +12,26 @@ export let view = new KWM_Route("/app", async function(){
 });
 
 view.rendering = async function(){
-    await kwm.render("app", document.getElementById("kwmJS"));
+    await kwm.render("app");
+
+    view.DOM = {
+        container: document.querySelector('#container'),
+        tasks: document.querySelector('#tasks'),
+        questions: document.querySelector('#questions'),
+    }
+
+    view.DOM.tasks.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const taskOverviewData = {};
+        await kwm.render('components/task-overview', view.DOM.container, taskOverviewData);
+        // TODO populate tasks
+    });
+
+    view.DOM.questions.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const questionOverviewData = {};
+        await kwm.render('components/question-overview', view.DOM.container, questionOverviewData);
+        // TODO populate filter
+        // TODO populate cards
+    });
 };
