6a330cd2992dfd489453373ff81066a25535349f
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2022-05-02 22:53:53 +0200

    Added component and for loop support to templating engine

--- app/js/kwm-templater.js
+++ app/js/kwm-templater.js
@@ -4,8 +4,11 @@
  *
  *     renderTemplate is a method to render any given Template into any given DOM-Container.
  *     Optional values argument serves for dynamic-data-storage.
- *     
- *     Translates %{key} and inserts &{key} values from passed in Data
+ *
+ *     - translates %{key}
+ *     - inserts values of &{key} passed in Data
+ *     - inserts other templates with <f:template>
+ *     - inserts a loop of other components with <f:for>
  *
  *     @author Jakob Osterberger - 2022-03-28
  *******************************************************/
@@ -14,27 +17,33 @@ export default class KWM_Templater {
         this.templatePath = templatePath;
         this.langRegex = /%{(.*?)}/g;
         this.valueRegex = /&{(.*?)}/g;
+        this.templRegex = /<f:template name="(.*?)"><\/f:template>/g;
+        this.forRegex = /<f:for var="(.*?)">(.*?)<\/f:for>/g;
     }
 
-    renderTemplate(templateName, container, values = false, mode='overwrite') {
-        return new Promise((resolve, reject) => {
-            fetch(this.templatePath + templateName + ".tpl?v=0.2")
-                .then(response => response.text())
-                .then(template => {
-                    if (!container || !container['innerHTML']) {
-                        reject(new Error('no valid container specified for render'));
-                    }
-                    if (!template || typeof template !== 'string') {
-                        reject(new Error('template invalid'));
-                    }
-                    if(mode === "append") container.innerHTML += this.translateTemplateValues(template, values);
-                    else container.innerHTML = this.translateTemplateValues(template, values);
-                    resolve();
-                });
-        });
+    async renderTemplate(templateName, container, values = false, mode = 'overwrite') {
+        if (!container) {
+            console.error(container);
+            throw new Error(`no valid container ${container} specified for rendering : '${templateName}'`);
+        }
+        const htmlString = await this.insertTemplate(templateName, values);
+        if (mode === "append") container.innerHTML += htmlString;
+        else if(mode === "fill") {
+            container.innerHTML = '';
+            container.innerHTML += htmlString;
+        }
+        else container.innerHTML = htmlString;
+        console.log(container);
     }
 
-    translateTemplateValues(template, values) {
+    async insertTemplate(templateName, values) {
+        const templateString = await fetch(this.templatePath + templateName + ".tpl?v=0.2").then(response => response.text());
+        if (!templateString || typeof templateString !== 'string')  throw new Error(`template '${templateName}' invalid: ${templateString}`);
+        const result = await this.translateTemplateValues(templateString, values);
+        return result;
+    }
+
+    async translateTemplateValues(template, values) {
         let result = template;
         for (const match of template.matchAll(this.langRegex)) {
             result = result.replace(match[0], kwm.translator.translate(match[1]));
@@ -42,6 +51,13 @@ export default class KWM_Templater {
         for (const match of template.matchAll(this.valueRegex)) {
             result = result.replace(match[0], this.insertValue(match[1], values));
         }
+        for (const match of template.matchAll(this.forRegex)) {
+            result = result.replace(match[0], await this.insertForLoop(match[1], values, match[2], 'html'));
+        }
+        for (const match of template.matchAll(this.templRegex)) {
+            result = result.replace(match[0], await this.insertTemplate(match[1], values));
+        }
+        console.log(result);
         return result;
     }
 
@@ -49,5 +65,27 @@ export default class KWM_Templater {
         if (!values) return ` --Missing data: values property is "${values}"-- `;
         if (!key in values) return ` --Missing data: non_existing_key "${key}"-- `;
         return values[key];
+    }
+
+    async insertForLoop(key, values = {}, templateName) {
+        let result = '';
+        if (kwm.utils.isEmpty(values)) throw new Error(` ---No values provided in Loop with '${key}'--- `);
+        if (typeof key !== 'string') throw new Error(` ---No valid key provided '${key}'--- `);
+        if (!key in values && kwm.utils.isEmpty(values[key])) {
+            console.error(values);
+            throw new Error(` ---Key is not provided in values: '${key}'--- `);
+        }
+        if (!templateName) throw new Error(` ---No valid template provided for Loop with '${key}'--- `);
+        else {
+            const array = values[key];
+            for (const values of array) {
+                try {
+                    result += await this.insertTemplate(templateName, values);
+                } catch (err) {
+                    throw new Error(err.message);
+                }
+            }
+        }
+        return result;
     }
 }
---
+++ app/templates/test.tpl
@@ -0,0 +1,3 @@
+<h1>Test Page</h1>
+<f:component templateName="components/test"></f:component>
+<f:for var="key">components/test</f:for>
